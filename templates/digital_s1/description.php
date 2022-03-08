50d30f36191cab6d9bbefdee0b38405e745b0b54
alekslesik <alekslesik@gmail.com>
2022-03-08 11:52:23 +0300

    add project

---
+++ templates/digital_s1/description.php
@@ -0,0 +1,11 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php
+
+use Bitrix\Main\Localization\Loc;
+
+$arTemplate = array (
+    'NAME' => Loc::getMessage('intec.digital.template.name'),
+    'DESCRIPTION' => Loc::getMessage('intec.digital.template.description'),
+    'SORT' => '',
+    'TYPE' => '',
+);
